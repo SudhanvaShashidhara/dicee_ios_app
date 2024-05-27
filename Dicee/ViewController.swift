8841d028dbf1b5592673e0010c60363565816154
Sudhanva Shashidhara <sudhanvashashidhara@gmail.com>
2024-05-27 23:26:26 +0530

    Dice Images and randomization added

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice_one_ref: UIImageView!
+    @IBOutlet weak var dice_two_ref: UIImageView!
+    
+    let dices_image_ref_array = [
+        UIImage(imageLiteralResourceName: "DiceOne"),
+        UIImage(imageLiteralResourceName: "DiceTwo"),
+        UIImage(imageLiteralResourceName: "DiceThree"),
+        UIImage(imageLiteralResourceName: "DiceFour"),
+        UIImage(imageLiteralResourceName: "DiceFive"),
+        UIImage(imageLiteralResourceName: "DiceSix")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let left_dice_number = Int.random(in: 0...5)
+        let right_dice_number = Int.random(in: 0...5)
+        print("Left Dice Number: \(left_dice_number)")
+        print("Right Dice Number: \(right_dice_number)")
+        dice_one_ref.image = dices_image_ref_array[left_dice_number]
+        dice_two_ref.image = dices_image_ref_array[right_dice_number]
 
+    }
+    
 }
 
